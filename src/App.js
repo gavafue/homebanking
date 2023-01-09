1127955ca60b32322d13ad18ddda87d5157d557a
Mati Agobian <mati.agobian@gmail.com>
2023-01-08 23:33:24 -0300

    Se crean carpetas necesarias para continuar la app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './components/Header/Header';
 import Login from './components/login/index';
 
 function App(){
   return <div className='app'>
+    <Header/>
     <Login />
     </div>
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Logo from '../../img/logo.png'
+const Header = () => {
+  return (
+    <div>
+      <img alt='logo' src={Logo}></img>
+    </div>
+  )
+}
+
+export default Header
